c680da02391fbb0a1a4f00a74643165453223d04
veysiadn <52937922+veysiadn@users.noreply.github.com>
2021-04-07 20:39:46 +0900

    HW3 Updated Final Version

--- HW3_VeysiADIN.jl
+++ HW3_VeysiADIN.jl
@@ -8,16 +8,18 @@ using LinearAlgebra
 # rotation about unit vector w by angle theta.
 # theta should be in radian
 function Rodrigues(w,theta)
-    if( size(w)!=(3,) )
-        println("!Warning! : W matrix should be 3x1.")
+    if( length(w) != 3)
+        println("!Warning! : W matrix should have 3 elements.")
         return
     end
-    Id_3x3=Diagonal(ones(3,3));
+    if(size(w)==(1,3))
+        w=w';
+    end
     w_hat = [0      -w[3]    w[2];
              w[3]     0     -w[1];
             -w[2]    w[1]      0];
-    Rotation_matrix = cos(theta)*Id_3x3 + sin(theta)*w_hat+(1-cos(theta))*w*w';
-    return Rotation_matrix;
+    rotation_matrix = cos(theta)*Matrix(1I,3,3) + sin(theta)*w_hat+(1-cos(theta))*w*w';
+    return rotation_matrix;
 end
 #****************************************************************#
 # Builds rigid transformation matrix T from given
@@ -27,13 +29,15 @@ function RpToTrans(R,t)
         println("!Warning! : Rotation matrix should be 3x3.")
         return
     end
-    if( size(t)!=(3,) )
-        println("!Warning! : Translation matrix should be 3x1.")
+    if( length(t) != 3 )
+        println("!Warning! : Translation matrix should have 3 elements.")
         return
     end
-    temp =  hcat(R,t) ;
-    row_4 = [0 0 0 1] ;
-    R_B_T = vcat(temp,row_4);
+    if(size(t)==(1,3))
+        t=t';
+    end
+    R_B_T = hcat(R,t) ;
+    R_B_T = vcat(R_B_T,[0 0 0 1]) ;
     return R_B_T
 end
 #****************************************************************#
@@ -52,13 +56,16 @@ end
 # Inverts rigid transformation matrix T
 function TransInv(T)
     if( size(T)!=(4,4) )
-        println("!Warning! : W matrix should be 3x1.")
+        println("!Warning! : W matrix should be 4x4.")
         return
     end
     if( det(T)==0 )
         println("!Warning! : Inverse doesn't exist (Determinant is equal to zero).")
         return
     end
-    return T^-1;
+    R,t = TransToRp(T);
+    r_inv =hcat(R',-(R'*t))
+    r_inv = vcat(r_inv,[0 0 0 1]);
+    return r_inv;
 end
 #****************************************************************#
